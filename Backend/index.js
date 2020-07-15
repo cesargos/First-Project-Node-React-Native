0f026f5fdb66c554c620834446db4e5f66b532c1
Cesar Goulart <devcesargoulart@gmail.com>
2020-07-15 17:28:38 -0300

    Middlewares

--- Backend/index.js
+++ Backend/index.js
@@ -1,40 +1,90 @@
 const express = require('express');
-const api = express();
+const { uuid, isUuid } = require('uuidv4');
 
-api.use(express.json());
+const app = express();
 
-const users = ['Cesar', 'Ana', 'Inho', 'Huddy'];
+app.use(express.json());
+
+const projects = [ ];
+
+//middleware Global
+function logRequest( req, res, next) {
+  const {method, url} = req;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.time('Request');
+  console.log( logLabel );
+  next(); //Chama o proximo middleware, usar return caso nao tenha codigo posterior
+  console.timeEnd('Request');
+}
+app.use(logRequest);
+
+//middleware global com rota
+function validateProjectId( req, res, next ) {
+  const { id } = req.params;
+  if (!isUuid(id)) {
+    return res.status(400).json({error:"Invalid project ID."});
+  }
+  return next();
+}
+app.use('/projects/:id', validateProjectId);
+
+//middleware local
+function checkProjectInArray(req, res, next) {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex( project => project.id === id);
+  
+  if (projectIndex < 0){
+    return res.status(400).json({error: "Project not found."});
+  };
+  req.index = projectIndex;
+  return next();
+}
 
 //CRUD - Create, Read, Update, Delete
-api.post('/users', (req, res) => {
-  const { name } = req.body;
-  users.push( name );
-  return res.json( users );
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body;
+  const project = {id: uuid(), title, owner}
+  projects.push( project );
+  return res.json( project );
 });
 
 
-api.get('/users', (req, res) => {
-  return res.json( users );
+app.get('/projects', (req, res) => {
+  const {title} = req.query;
+  
+  const results = title 
+    ? projects.filter( project =>project.title.includes(title))
+    : projects;
+
+  return res.json( results );
 });
 
-api.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-  return res.json( users[ index ]);
+app.get('/projects/:id', checkProjectInArray, (req, res) => {
+  const { id } = req.params;
+
+  return res.json( projects[req.index]);
 });
 
-api.put('/users/:index', (req, res) => {
-  const {index} = req.params;
-  const {name} = req.body;
-  users[index] = name;
-  return res.send().status(204);
+app.put('/projects/:id', checkProjectInArray, (req, res) => {
+  const { id } = req.params;
+  const {title, owner} = req.body;
+  
+  const project = {id, title, owner };
+  projects[req.index] =  project;
+
+  return res.json( projects[ req.index ]);
 });
 
-api.delete('/users/:index', (req, res) => {
-  const {index} = req.params;
-  users.splice(index, 1);
-  return res.send().status(204);
+app.delete('/projects/:id', checkProjectInArray, (req, res) => {
+  const {id} = req.params;
+
+  projects.splice(req.index, 1);
+  return res.status(204).send();
 });
 
 
 
-api.listen(3000);
+app.listen(3333, () => {
+  console.log("🚀 Back-end started 🚀");
+});
